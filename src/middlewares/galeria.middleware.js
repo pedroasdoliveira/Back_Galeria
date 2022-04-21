400c8c757bf1e82f673b0e2c16fdf46d575eb919
Pedro Augusto Silva de Oliveira <89483941+pedroasdoliveira@users.noreply.github.com>
2022-04-20 23:17:33 -0300

    erro na verificação

--- src/middlewares/galeria.middleware.js
+++ src/middlewares/galeria.middleware.js
@@ -1,5 +1,13 @@
 import mongoose from 'mongoose';
 
 export const validId = (req, res, next) => {
-    
-}
+  const idParam = req.param.id;
+  console.log(idParam)
+
+  // erro idParam == undefined
+  if (!mongoose.Types.ObjectId.isValid(idParam)) {
+    return res.status(400).send({ message: 'Id invalido' });
+  }
+
+  next();
+};
