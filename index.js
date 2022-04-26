9b93f1ff836e1b3a7e41db5efb8fd8f525911426
Pedro Augusto Silva de Oliveira <89483941+pedroasdoliveira@users.noreply.github.com>
2022-04-26 13:37:17 -0300

    configurações para o mongodb atlas

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
+import dotenv from 'dotenv';
 import express from "express";
 import cors from "cors";
 import { route } from './src/routes/galeria.routes.js';
 import { connectToDatabase } from "./src/database/database.js";
 // --------------------------------------------------- Imports -------------------------------
 
+dotenv.config();
 const app = express();
 connectToDatabase();
 
@@ -12,7 +14,7 @@ app.use(cors());
 app.use('/galleries', route);
 
 // ------------------------------------------- Porta ---------------------------------
-const port = 3005;
+const port = process.env.PORT || 3005;
 
 app.listen(port, () => {
     console.log(`Aplicação rodando na porta ${port} do servidor.`);
--- src/database/database.js
+++ src/database/database.js
@@ -4,13 +4,13 @@ import mongoose from 'mongoose';
 
 export const connectToDatabase = () => {
   mongoose
-    .connect('mongodb://localhost:27017/galerias-db', {
+    .connect(process.env.URI_DATABASE, {
       // encontrar na documentação
       useNewUrlParser: true,
       useUnifiedTopology: true,
     })
     .then(() => {
-      console.log('MongoDb Connect!');
+      console.log('MongoDb Atlas Connect!');
     })
     .catch((err) => {
       console.log(`Erro ao conectar ao servidor. Erro: ${err}.`);
